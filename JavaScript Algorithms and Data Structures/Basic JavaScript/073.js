ad95fd45b092c2125342b20f28c14ff8ae93683a
reninjad <reninjad@gmail.com>
2022-05-25 00:58:27 +0300

    80 from 113

---
+++ JavaScript Algorithms and Data Structures/Basic JavaScript/073.js
@@ -0,0 +1,26 @@
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+	// Only change code below this line
+	let result = '';
+	if (strokes === 1) {
+		result = names[0];
+	} else if (strokes <= par - 2) {
+		result = names[1];
+	} else if (strokes === par - 1) {
+		result = names[2];
+	} else if (strokes === par) {
+		result = names[3];
+	} else if (strokes === par + 1) {
+		result = names[4];
+	} else if (strokes === par + 2) {
+		result = names[5];
+	} else if (strokes >= par + 3) {
+		result = names[6];
+	}
+
+	return result;
+	// Only change code above this line
+}
+
+golfScore(5, 4);
