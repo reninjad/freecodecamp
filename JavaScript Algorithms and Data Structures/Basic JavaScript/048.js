a32ed31e5c0ba252ca5316f7e245a478f83f724c
reninjad <reninjad@gmail.com>
2022-05-24 13:52:45 +0300

    50 from 113

---
+++ JavaScript Algorithms and Data Structures/Basic JavaScript/048.js
@@ -0,0 +1,5 @@
+function functionWithArgs(paramOne, paramTwo) {
+	console.log(paramOne + paramTwo);
+}
+functionWithArgs(1, 2);
+functionWithArgs(7, 9);
---
+++ JavaScript Algorithms and Data Structures/Basic JavaScript/049.js
@@ -0,0 +1,6 @@
+function timesFive(number) {
+	return number * 5;
+}
+timesFive(5);
+timesFive(2);
+timesFive(0);
