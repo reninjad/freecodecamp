78e47a6ce3739537eb9b6524d6dc9f486fe1024f
reninjad <reninjad@gmail.com>
2022-05-29 00:49:02 +0300

    100% basic

---
+++ JavaScript Algorithms and Data Structures/Basic JavaScript/102.js
@@ -0,0 +1,44 @@
+// Setup
+const contacts = [
+	{
+		firstName: "Akira",
+		lastName: "Laine",
+		number: "0543236543",
+		likes: ["Pizza", "Coding", "Brownie Points"],
+	},
+	{
+		firstName: "Harry",
+		lastName: "Potter",
+		number: "0994372684",
+		likes: ["Hogwarts", "Magic", "Hagrid"],
+	},
+	{
+		firstName: "Sherlock",
+		lastName: "Holmes",
+		number: "0487345643",
+		likes: ["Intriguing Cases", "Violin"],
+	},
+	{
+		firstName: "Kristian",
+		lastName: "Vos",
+		number: "unknown",
+		likes: ["JavaScript", "Gaming", "Foxes"],
+	},
+];
+
+function lookUpProfile(name, prop) {
+	// Only change code below this line
+	let contact = contacts.find(item => item.firstName == name);
+	if (contact !== undefined) {
+		if (prop in contact) {
+			return contact[prop];
+		} else {
+			return 'No such property';
+		}
+	} else {
+		return 'No such contact'
+	}
+	// Only change code above this line
+}
+
+lookUpProfile("Akira", "likes");
