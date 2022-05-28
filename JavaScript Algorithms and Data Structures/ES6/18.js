f90d048ecabc8f0cb1d9aef6afaa2663da2debdf
reninjad <reninjad@gmail.com>
2022-05-29 02:12:57 +0300

    100% ES6

---
+++ JavaScript Algorithms and Data Structures/ES6/18.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+class Vegetable {
+	constructor(vegetable) {
+		this.name = vegetable;
+	}
+}
+// Only change code above this line
+
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // Should display 'carrot'
